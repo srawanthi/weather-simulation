0aa3b1c3ed31dd655af8ed7585a0c7ac74cff5dd
srawanthi <srawanthi.samala.consultant@nielsen.com>
2017-07-17 09:18:50 +0530

    code format changes

--- src/main/java/com/tcs/weather/bo/WeatherDataParser.java
+++ src/main/java/com/tcs/weather/bo/WeatherDataParser.java
@@ -13,15 +13,16 @@ import com.tcs.weather.constants.WeatherConstants;
 import com.tcs.weather.dto.WeatherDataDto;
 
 /**
- * @author srawanthi.samala
- * WeatherDataParser parse Weather input data by considering 2 input files as timezoneFileLookupPath, weatherDataInputPath
- * and generates WeatherDataDto
+ * @author srawanthi.samala WeatherDataParser parse Weather input data by
+ *         considering 2 input files as timezoneFileLookupPath,
+ *         weatherDataInputPath and generates WeatherDataDto
  */
 public class WeatherDataParser {
 
 	/**
-	 * parseWeatherTimezoneRecord parse the weather data input line by lookup timezone map
-	 * and generates the WeatherDataDto with required parameters
+	 * parseWeatherTimezoneRecord parse the weather data input line by lookup
+	 * timezone map and generates the WeatherDataDto with required parameters
+	 * 
 	 * @param weatherline
 	 * @param tzlonglatMap
 	 * @return WeatherDataDto
@@ -37,12 +38,15 @@ public class WeatherDataParser {
 	}
 
 	/**
-	 * parseWeatherRecord parse weather data input line and generated WeatherDataDto 
+	 * parseWeatherRecord parse weather data input line and generated
+	 * WeatherDataDto
+	 * 
 	 * @param weatherline
 	 * @return WeatherDataDto
 	 */
 	public WeatherDataDto parseWeatherRecord(String weatherline) {
-		String[] weathersplit = weatherline.split(WeatherConstants.ROW_SPLIT_SEPERATOR);
+		String[] weathersplit = weatherline
+				.split(WeatherConstants.ROW_SPLIT_SEPERATOR);
 		WeatherDataDto objWeatherDto = new WeatherDataDto();
 		if (weathersplit.length == 7) {
 			objWeatherDto.setLocation(weathersplit[0]);
@@ -57,7 +61,9 @@ public class WeatherDataParser {
 	}
 
 	/**
-	 * parseTimeZoneRecord lookup data into timezone file and generated the required fields in WeatherDataDto
+	 * parseTimeZoneRecord lookup data into timezone file and generated the
+	 * required fields in WeatherDataDto
+	 * 
 	 * @param longlatrec
 	 * @param objWeatherDto
 	 * @return WeatherDataDto
@@ -66,36 +72,48 @@ public class WeatherDataParser {
 	public WeatherDataDto parseTimeZoneRecord(String longlatrec,
 			WeatherDataDto objWeatherDto) throws ParseException {
 
-		String[] longlatsplit = longlatrec.split(WeatherConstants.ROW_SPLIT_SEPERATOR);
+		String[] longlatsplit = longlatrec
+				.split(WeatherConstants.ROW_SPLIT_SEPERATOR);
 		if (longlatsplit.length == 3) {
 			objWeatherDto.setCity(longlatsplit[1]);
 			objWeatherDto.setTimezone(longlatsplit[2]);
 		}
-		//UTC standard timezone to running Operating system timezone
-		SimpleDateFormat dateformater = new SimpleDateFormat(WeatherConstants.TIME_yyyy_MM_dd_T_HH_mm_ss_Z);
-		dateformater.setTimeZone(TimeZone.getTimeZone(WeatherConstants.TIME_UTC_TIMEZONE));
+		// UTC standard timezone to running Operating system timezone
+		SimpleDateFormat dateformater = new SimpleDateFormat(
+				WeatherConstants.TIME_yyyy_MM_dd_T_HH_mm_ss_Z);
+		dateformater.setTimeZone(TimeZone
+				.getTimeZone(WeatherConstants.TIME_UTC_TIMEZONE));
 		Date timeZoneDate = dateformater.parse(objWeatherDto.getLocaltimeutc());
 		TimeZone tz = TimeZone.getTimeZone(objWeatherDto.getTimezone());
-		//System.out.println("UTC standard timezone "+objWeatherDto.getLocaltimeutc());
-		// running Operating system timezone to specified timezone in lookup timezone data
-		SimpleDateFormat formatter = new SimpleDateFormat(WeatherConstants.TIME_yyyy_MM_dd_T_HH_mm_ss_Z);
-		formatter.setTimeZone(TimeZone.getTimeZone(objWeatherDto.getTimezone()));
-		
+		// System.out.println("UTC standard timezone "+objWeatherDto.getLocaltimeutc());
+		// running Operating system timezone to specified timezone in lookup
+		// timezone data
+		SimpleDateFormat formatter = new SimpleDateFormat(
+				WeatherConstants.TIME_yyyy_MM_dd_T_HH_mm_ss_Z);
+		formatter
+				.setTimeZone(TimeZone.getTimeZone(objWeatherDto.getTimezone()));
+
 		String dataDateString = formatter.format(timeZoneDate);
 		objWeatherDto.setLocaltime(dataDateString);
-		
-		//System.out.println("specified timezone "+objWeatherDto.getLocaltime());		
+
+		// System.out.println("specified timezone "+objWeatherDto.getLocaltime());
 
 		String yyyyMMdd = dataDateString.split("T")[0];
 		String HHmmss = dataDateString.split("T")[1].split("Z")[0];
-		//converts localdate to LocalDateTime to find out timeoffset value in UTC
-		LocalDateTime dt = LocalDateTime.of(
-				Integer.parseInt(yyyyMMdd.split(WeatherConstants.DATE_FIELD_SEPERATOR)[0]),
-				Integer.parseInt(yyyyMMdd.split(WeatherConstants.DATE_FIELD_SEPERATOR)[1]),
-				Integer.parseInt(yyyyMMdd.split(WeatherConstants.DATE_FIELD_SEPERATOR)[2]),
-				Integer.parseInt(HHmmss.split(WeatherConstants.TIME_FIELD_SEPERATOR)[0]),
-				Integer.parseInt(HHmmss.split(WeatherConstants.TIME_FIELD_SEPERATOR)[1]),
-				Integer.parseInt(HHmmss.split(WeatherConstants.TIME_FIELD_SEPERATOR)[2]));
+		// converts localdate to LocalDateTime to find out timeoffset value in
+		// UTC
+		LocalDateTime dt = LocalDateTime.of(Integer.parseInt(yyyyMMdd
+				.split(WeatherConstants.DATE_FIELD_SEPERATOR)[0]),
+				Integer.parseInt(yyyyMMdd
+						.split(WeatherConstants.DATE_FIELD_SEPERATOR)[1]),
+				Integer.parseInt(yyyyMMdd
+						.split(WeatherConstants.DATE_FIELD_SEPERATOR)[2]),
+				Integer.parseInt(HHmmss
+						.split(WeatherConstants.TIME_FIELD_SEPERATOR)[0]),
+				Integer.parseInt(HHmmss
+						.split(WeatherConstants.TIME_FIELD_SEPERATOR)[1]),
+				Integer.parseInt(HHmmss
+						.split(WeatherConstants.TIME_FIELD_SEPERATOR)[2]));
 		ZonedDateTime zdt = dt.atZone(tz.toZoneId());
 		ZoneOffset zos = zdt.getOffset();
 		// replace Z to +00:00
--- src/main/java/com/tcs/weather/bo/WeatherDataUtils.java
+++ src/main/java/com/tcs/weather/bo/WeatherDataUtils.java
@@ -15,18 +15,21 @@ import com.tcs.weather.dto.WeatherDataDto;
 
 /**
  * 
- * @author srawanthi.samala
- * WeatherDataUtils contains all methods related to File Content reading and convert into required object
+ * @author srawanthi.samala WeatherDataUtils contains all methods related to
+ *         File Content reading and convert into required object
  */
 public class WeatherDataUtils {
-	
+
 	/**
-	 * getFileContentinBuffer read weather data file content and generate bufferreader object
+	 * getFileContentinBuffer read weather data file content and generate
+	 * bufferreader object
+	 * 
 	 * @param path
 	 * @return BufferedReader
 	 * @throws FileNotFoundException
 	 */
-	public BufferedReader getFileContentinBuffer(String path) throws FileNotFoundException{
+	public BufferedReader getFileContentinBuffer(String path)
+			throws FileNotFoundException {
 		File initialFile = new File(path);
 		InputStream in = new FileInputStream(initialFile);
 		BufferedReader br = new BufferedReader(new InputStreamReader(in));
@@ -34,40 +37,51 @@ public class WeatherDataUtils {
 	}
 
 	/**
-	 * convertTimezoneDatatoMap reads timezone data file content and generates HashMap<String, String>
-	 * HashMap<String, String> contains key as longitude latitude value as position, country, timezone
+	 * convertTimezoneDatatoMap reads timezone data file content and generates
+	 * HashMap<String, String> HashMap<String, String> contains key as longitude
+	 * latitude value as position, country, timezone
+	 * 
 	 * @param path
 	 * @return HashMap<String, String>
 	 * @throws IOException
 	 */
-	public HashMap<String, String> convertTimezoneDatatoMap(String path) throws IOException {
+	public HashMap<String, String> convertTimezoneDatatoMap(String path)
+			throws IOException {
 		HashMap<String, String> timezoneMap = new HashMap<String, String>();
 		// -33.86,151.21,39|Sydney|Australia/Sydney
 
-		BufferedReader br1 =getFileContentinBuffer(path);
+		BufferedReader br1 = getFileContentinBuffer(path);
 
 		String line1 = br1.readLine();
 		while (line1 != null) {
-			if (!timezoneMap.containsKey(line1.split(WeatherConstants.ROW_SPLIT_SEPERATOR)[0]))
-				timezoneMap.put(line1.split(WeatherConstants.ROW_SPLIT_SEPERATOR)[0], line1);
+			if (!timezoneMap.containsKey(line1
+					.split(WeatherConstants.ROW_SPLIT_SEPERATOR)[0]))
+				timezoneMap.put(
+						line1.split(WeatherConstants.ROW_SPLIT_SEPERATOR)[0],
+						line1);
 			line1 = br1.readLine();
 		}
 		return timezoneMap;
 	}
-	
+
 	/**
-	 * convertWeatherDatatoMap converts the input weather data by lookup timezonemap into Hashmap of String and WeatherDataDTo
-	 * input weather data contains location|position|localtimeutc|conditions|temparature|position|humidity 
+	 * convertWeatherDatatoMap converts the input weather data by lookup
+	 * timezonemap into Hashmap of String and WeatherDataDTo input weather data
+	 * contains
+	 * location|position|localtimeutc|conditions|temparature|position|humidity
+	 * 
 	 * @param path
 	 * @param timezoneMap
 	 * @return HashMap<String, WeatherDataDto>
 	 * @throws IOException
 	 * @throws ParseException
 	 */
-	public HashMap<String, WeatherDataDto> convertWeatherDatatoMap(String path,HashMap<String, String> timezoneMap) throws IOException, ParseException {
+	public HashMap<String, WeatherDataDto> convertWeatherDatatoMap(String path,
+			HashMap<String, String> timezoneMap) throws IOException,
+			ParseException {
 		HashMap<String, WeatherDataDto> weatherMap = new HashMap<String, WeatherDataDto>();
 
-		BufferedReader br =getFileContentinBuffer(path);
+		BufferedReader br = getFileContentinBuffer(path);
 
 		String line = br.readLine();
 		while (line != null) {
@@ -75,12 +89,14 @@ public class WeatherDataUtils {
 			WeatherDataDto objWeatherDataDto = objParser
 					.parseWeatherTimezoneRecord(line, timezoneMap);
 			weatherMap.put(
-					objWeatherDataDto.getCity() + WeatherConstants.ROW_SEPERATOR
-							+ objWeatherDataDto.getDateval() + WeatherConstants.ROW_SEPERATOR
+					objWeatherDataDto.getCity()
+							+ WeatherConstants.ROW_SEPERATOR
+							+ objWeatherDataDto.getDateval()
+							+ WeatherConstants.ROW_SEPERATOR
 							+ objWeatherDataDto.getTime(), objWeatherDataDto);
 			line = br.readLine();
 		}
-		
+
 		return weatherMap;
 	}
 }
